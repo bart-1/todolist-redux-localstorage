5cdec51f7abc2d583051a7b9d050d259c52883e3
Bartosz Woldański <bart@dev-1.pl>
2021-10-17 20:45:01 +0200

    add mockup of Task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Task = () => {
+    return (
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+     );
+}
+ 
+export default Task;

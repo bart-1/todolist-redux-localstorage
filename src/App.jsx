0fadb3dbf59a16c9e9d0cc75c356a8747500a749
Bartosz Woldański <bart@dev-1.pl>
2021-10-17 15:39:28 +0200

    Add Provider wraper with store to main div named App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
+import store from './store/store';
 import './styles/App.css';
 
 function App() {
 
   return (
-    <div className="App">
-      to-do-list
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        to-do-list
+      </div>
+    </Provider>
   )
 }
 

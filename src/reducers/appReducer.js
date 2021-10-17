d5752763c68e2e56fe33d039414ed580e9eacd04
Bartosz Woldański <bart@dev-1.pl>
2021-10-17 16:51:38 +0200

    appReducers switch

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,3 +1,43 @@
+import {
+    ADD_TASK,
+    CHANGE_TASKS_STATUS,
+    DELETE_TASK,
+    EDIT_TASK,
+} from "../actions/appActions";
+
 export const appReducer = (state = [], actions) => {
-    return state
+    switch (actions.type) {
+        case ADD_TASK:
+            return [...state, actions.payload];
+        case CHANGE_TASKS_STATUS:
+            return state.map(task => {
+                if (actions.payload.taskId === task.id) {
+                    return task = {
+                        taskBody,
+                        taskDate,
+                        taskId,
+                        taskStatus: !taskStatus,
+                    }
+                }
+                return task;
+            });
+        case DELETE_TASK:
+            return state.filter(actions.payload.taskId !== task.id);
+        case EDIT_TASK:
+            return state.map(task => {
+                if (actions.payload.taskId === task.id) {
+                    return task = {
+                        taskBody: actions.payload.taskBody,
+                        taskDate: actions.payload.taskDate,
+                        taskId,
+                        taskStatus,
+                    };
+                }
+                return task;
+            });
+        default:
+            console.warn(`unknown type: ${actions.type}`);
+            return state;
+
+    }
 };
